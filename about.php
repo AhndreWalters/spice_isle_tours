39651f7105b881ddf7c14a05677c5a8491183c67
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-11-30 05:03:12 +0000

    Update about.php

--- about.php
+++ about.php
@@ -11,7 +11,6 @@ require_once 'auth_check.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/navigation.css">
 </head>
 <body>
     <?php include 'includes/navigation.php'; ?>

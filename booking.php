f63b137d43ae461922614b61963bb7dc94f1ba86
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-11-30 05:15:54 +0000

    Update booking.php

--- booking.php
+++ booking.php
@@ -32,7 +32,6 @@ if (isset($_SESSION['form_data'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/navigation.css">
 </head>
 <body>
     <?php include 'includes/navigation.php'; ?>
